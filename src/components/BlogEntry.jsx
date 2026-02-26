63dd762e38e1aff672f834a509e9682ad4f89735
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-26 22:42:32 +0100

    Added basic box styles

--- src/components/BlogEntry.jsx
+++ src/components/BlogEntry.jsx
@@ -1,19 +1,19 @@
 function BlogEntry({ title, description, date }) {
   return (
-    <div className="card">
-      <h2>{title}</h2>
-      <h5>
-        {description}, {date}
-      </h5>
-      <div className="fakeimg" style={{ height: "200px" }}>
-        Image
+    <div className="blogcard">
+      <div className="innerblogcard">
+        <h2>{title}</h2>
+        <h5>
+          {description}, {date}
+        </h5>
+        <div className="thumbnailimg">Image</div>
+        <p>Some text..</p>
+        <p>
+          Sunt in culpa qui officia deserunt mollit anim id est laborum
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.
+        </p>
       </div>
-      <p>Some text..</p>
-      <p>
-        Sunt in culpa qui officia deserunt mollit anim id est laborum
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua.
-      </p>
     </div>
   );
 }
