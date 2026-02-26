ac8e6ddcb53df260294f6a8c2e5a311e9fc83280
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-27 00:41:19 +0100

    trying to fix import of glitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Header from "./components/Header";
 import BlogLayout from "./components/BlogLayout";
 import Footer from "./components/Footer";
-import Glitch from "./components/glitch";
 import "./styles/blog.css";
 import "./styles/effects.css";
 
