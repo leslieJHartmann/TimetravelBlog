3b370b1fae214706cdba73779b4d6ef653750134
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-28 12:56:50 +0100

    Fixing site styling and clipping of cards

--- src/components/BlogLayout.jsx
+++ src/components/BlogLayout.jsx
@@ -6,7 +6,7 @@ function BlogLayout() {
     <div className="row">
       <div className="leftcolumn">
         <BlogEntry
-          title="GOODBYE WORLD."
+          title="Goodbye World."
           date="14.08.2229"
           author="P4r4D0x"
           description={
@@ -15,7 +15,7 @@ function BlogLayout() {
         />
 
         <BlogEntry
-          title="DISCOVERY."
+          title="Discovery."
           date="28.01.2230"
           author="P4r4D0x"
           description="Gotta admit... I thought it was beyond me to be surprised anymore.
@@ -24,12 +24,10 @@ function BlogLayout() {
         />
 
         <BlogEntry
-          title="DISCOVERY."
-          date="28.01.2230"
-          author="P4r4D0x"
-          description="Gotta admit... I thought it was beyond me to be surprised anymore.
-                      Or maybe I’m hallucinating. It beats staring into space, so we’re investigating. This... Anomaly. Thought time weirdness was a sci-fi thing. People made it up.
-                      Well, if you’re reading this, I guess that can’t be true."
+          title="ERROR. Entry not found."
+          date="Data not found"
+          author="Data not found"
+          description="01000110011100100110000101100111011011010110010101101110011101000010111000100000010011000110111101110011011101000010111000100000010001100110111101110101011011100110010000101110001000000101000001100001011100100110000101100100011011110111100000101110000010100000101001011011010000010100001101000011010001010101001101010011001000000100010001000101010011100100100101000101010001000101110100001010"
         />
       </div>
 
