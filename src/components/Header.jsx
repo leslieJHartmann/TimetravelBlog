871cae2a02aba9ad5ea20cda2772cbddae4d4a8c
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-28 14:52:30 +0100

    added header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,14 @@ import Glitch from "./Glitch.jsx";
 function Header() {
   return (
     <div className="header">
+      {" "}
+      <Glitch auto burstOnHover sound>
+        <img
+          className="headerLogo"
+          src="Logo_noGlitch.svg"
+          alt="Logo"
+        ></img>{" "}
+      </Glitch>
       <h2>
         <Glitch auto>TEA & TIME</Glitch>
       </h2>
