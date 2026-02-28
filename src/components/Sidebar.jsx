b9f7085ba53176323084e0aa3b9c92e936b5f109
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-28 20:23:50 +0100

    Customising sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,18 @@
+import Logo from "./src/assets/Logo.svg?react";
+
 function Sidebar() {
   return (
     <div className="rightcolumn">
-      <div className="card">
-        <h2>About</h2>
-        <div className="fakeimg" style={{ height: "100px" }}>
-          Image
+      <div className="sideCard">
+        <h2>[ DATA NOT FOUND ]</h2>
+        <p>Welcome, [ User Unknown ]</p>
+        <div>
+          <Logo></Logo>
         </div>
-        <p>Some text about me...</p>
-      </div>
-
-      <div className="card">
-        <h2>Notes</h2>
-        <div className="fakeimg">Image</div>
         <br />
         <div className="fakeimg">Image</div>
         <br />
         <div className="fakeimg">Image</div>
-      </div>
-
-      <div className="card">
-        <h3>Follow Me</h3>
-        <p>Some text..</p>
       </div>
     </div>
   );
