ef9a020657bf48b1dc69ee925df39d92c1d26b3d
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-28 13:38:46 +0100

    fixed card text alignments

--- src/components/BlogEntry.jsx
+++ src/components/BlogEntry.jsx
@@ -4,26 +4,27 @@ import Glitch from "./Glitch";
 
 function BlogEntry({ title, description, date, author }) {
   return (
-    <div>
-      <div className="blogcard">
-        <img
-          src="/images/thumbnail1.png"
-          alt="Hand on keyboard"
-          className="thumbnailimg"
-        />
-        <div className="innerblogcard">
-          <div className="title-wrapper">
-            <Glitch burstOnHover sound>
-              <h2 className="blogEntryTitle">{title}</h2>
-            </Glitch>
-          </div>
+    <div className="blogcard">
+      <img
+        src="/images/thumbnail1.png"
+        alt="Hand on keyboard"
+        className="thumbnailimg"
+      />
+
+      <div className="innerblogcard">
+        <div className="entryHeader">
+          <Glitch burstOnHover sound>
+            <h2 className="blogEntryTitle">{title}</h2>
+          </Glitch>
+
           <h5 className="blogEntrySub">
             {date}
-            <br></br>
+            <br />
             {author}
           </h5>
-          <p className="blogEntryDesc">{description}</p>
         </div>
+
+        <p className="blogEntryDesc">{description}</p>
       </div>
     </div>
   );
