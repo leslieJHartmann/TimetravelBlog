6589bbaf0a86b37a4da511e1353c9498981f5a73
leslieJHartmann <73957222+leslieJHartmann@users.noreply.github.com>
2026-02-28 17:32:34 +0100

    added ambient noise to blog page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Header from "../components/Header";
 import BlogLayout from "../components/BlogLayout";
 import Footer from "../components/Footer";
@@ -5,6 +6,22 @@ import "../styles/blog.css";
 import "../styles/effects.css";
 
 function Home() {
+  const ambientRef = useRef(null);
+
+  // low volume ambient sound
+  useEffect(() => {
+    const audio = new Audio("/sfx/ambientLoop.mp3");
+    audio.loop = true;
+    audio.volume = 0.5;
+    audio.play().catch(() => {});
+    ambientRef.current = audio;
+
+    return () => {
+      audio.pause();
+      audio.currentTime = 0;
+    };
+  }, []);
+
   return (
     <>
       <div className="container">
